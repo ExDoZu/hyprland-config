b8b7224049581caadad8920f292c1baff6e522f0
ExDo <donotwritetome>
2024-12-29 18:20:26 +0300

    init

---
+++ commands.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp -r pictures/. ~/Pictures/hyprland-config-pictures
+sudo cp pictures/grub/grub.png /boot/grub/grub.png
+
+cp -r hypr ~/.config/
+cp -r kitty ~/.config/
+cp -r Thunar ~/.config/
+cp -r waybar ~/.config/
+
